bdccab0ce008e782e45e1532e686803c3ebe7819
Andrej Fedoseev <fedoseevandrej055@gmail.com>
2026-03-27 19:02:46 +0400

    Fix: Clean code analysis - remove unused imports, fix test, optimize performance

--- lib/services/audio_player_service.dart
+++ lib/services/audio_player_service.dart
@@ -1,6 +1,5 @@
 import 'package:just_audio/just_audio.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'dart:async';
 import '../models/song_model.dart';
